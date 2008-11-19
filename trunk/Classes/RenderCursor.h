9bcc31e1540a51ae4f7568ad68981cc5fc2996b9
songuke <songuke@0c176014-961b-11dd-a59c-95bcd8c322ac>
2008-11-19 13:58:02 +0000

    Add missing files from previous commit.

---
+++ trunk/Classes/RenderCursor.h
@@ -0,0 +1,17 @@
+//
+//  RenderCursor.h
+//  JouzuGomoku
+//
+//  Created by Son Hua on 11/16/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RenderCell.h"
+
+@interface RenderCursor : RenderCell {
+
+}
+- (void)render:(CGContextRef)context;
+
+@end
---
+++ trunk/Classes/RenderCursor.m
@@ -0,0 +1,39 @@
+//
+//  RenderCursor.m
+//  JouzuGomoku
+//
+//  Created by Son Hua on 11/16/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "RenderCursor.h"
+
+
+@implementation RenderCursor
+
+float cursorFillColor[] = {0.92, 0.92, 0.92, 1.0};
+
+// override
+- (void)render:(CGContextRef)context {
+	CGContextSaveGState(context);
+	// draw a rect
+	
+	 CGRect fillRect = CGRectMake(0, 0, cellSize, cellSize);		
+	 CGColorRef fillColorRef = CGColorCreate(CGColorSpaceCreateDeviceRGB(), cursorFillColor);
+	 CGContextBeginPath(context);
+	 CGContextAddRect(context, fillRect);	
+	 CGContextSetFillColorWithColor(context, fillColorRef);
+	 CGContextFillPath(context);
+	 
+	
+	CGRect rect = CGRectMake(0, 0, cellSize+1, cellSize+1);
+	CGContextSetRGBStrokeColor(context, 0.5, 0.5, 0.5, 1.0);
+	CGContextBeginPath(context);
+	CGContextAddRect(context, rect);
+	CGContextStrokePath(context); // draw current path
+		
+	CGContextRestoreGState(context);
+	
+}
+
+@end
