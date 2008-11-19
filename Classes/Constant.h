62fd55f08221df568bed5a136f40e72959285adb
songuke <songuke@0c176014-961b-11dd-a59c-95bcd8c322ac>
2008-11-19 13:58:02 +0000

    Add missing files from previous commit.

---
+++ Classes/Constant.h
@@ -0,0 +1,13 @@
+/*
+ *  Constant.h
+ *  JouzuGomoku
+ *
+ *  Created by Son Hua on 11/17/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#define VIEW_BOARD_SIZE 10
+#define DEFAULT_BOARD_SIZE 19
+#define DEFAULT_SEARCH_DEPTH 6
+
