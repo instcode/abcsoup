1d95dd7ecb7927e11653789f6132b5f7529eadbb
songuke <songuke@0c176014-961b-11dd-a59c-95bcd8c322ac>
2009-09-26 17:18:55 +0000

    TextureManager and ImageLoader class added.

---
+++ jigsaw/Classes/ImageLoader.h
@@ -0,0 +1,38 @@
+//
+//  ImageLoader.h
+//  Jigsaw
+//
+//  Created by Son Hua on 26/09/09.
+//  Copyright 2009 AptusVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+unsigned char* GetImagePixelData(CGImageRef inImage);
+
+@interface ImageLoader : NSObject {
+@private
+	NSMutableDictionary* dictImage; // keep track of the number of loaded images
+}
+
+/**
+ Singleton
+ */
++ (ImageLoader*) instance;
+
+/**
+ Load an image into current dictionary
+ */
+- (CGImageRef) loadImage: (NSString*) file;
+
+/**
+ Render CGImage into a RGBA context and read back the pixels
+ */
+- (unsigned char*) getImagePixelData: (CGImageRef) inImage: (int) channels;
+
+/**
+ Save to PPM format (for debug)
+ */
+- (void) saveImage: (CGImageRef) inImage: (NSString*) file;
+@end
