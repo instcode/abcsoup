e016901e1d511a84b3c03884bb597049cfdabd90
songuke <songuke@0c176014-961b-11dd-a59c-95bcd8c322ac>
2009-12-08 16:28:00 +0000

    If snap fails then revert.
    Tray compaction using prefix sum.

---
+++ trunk/jigsaw/Classes/RenzoUtil.h
@@ -0,0 +1,17 @@
+/*
+ *  RenzoUtil.h
+ *  Jigsaw
+ *
+ *  Created by Son Hua on 07/12/09.
+ *  Copyright 2009 AptusVentures. All rights reserved.
+ *
+ */
+
+namespace Renzo
+{
+	void randomSeed(int seed = 0);
+	int randomInteger(int min, int max);
+	
+	void swap(int& a, int& b);
+}
+
